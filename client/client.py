1ac390d04aade7cf42024b85eb6f281a95016992
BastLast <bastien.last87@gmail.com>
2021-01-27 17:30:23 +0100

    Client support for commands

--- client/client.py
+++ client/client.py
@@ -6,7 +6,11 @@ import socket
 import sys
 import threading
 import yaml
-from commands import get, put, ls, perm, rm
+from commands.get import get
+from commands.put import put
+from commands.ls import ls
+from commands.perm import perm
+from commands.rm import rm
 
 host = '127.0.0.1'
 port = 40000
@@ -31,30 +35,6 @@ class ThreadReception(threading.Thread):
         self.connexion.close()
 
 
-def parseargs(message):
-    m_quotes = message.split("\"")
-    m_parsed = []
-    i = 0
-    for m in m_quotes:
-        if i:
-            m_parsed.append(m)
-        else:
-            m_parsed += m.split(" ")
-        i += 1
-    m_parsed = [m for m in m_parsed if m != ""]
-    return m_parsed
-
-
-def exec_command(args):
-    return {
-        'get': get.get(args),
-        'put': put.put(args),
-        'ls': ls.ls(args),
-        'rm': rm.rm(args),
-        'perm': perm.perm(args)
-    }.get(args[0], "Unrecognized command.")
-
-
 class ThreadEmission(threading.Thread):
 
     # Thread envoyant les commandes
@@ -63,14 +43,39 @@ class ThreadEmission(threading.Thread):
         threading.Thread.__init__(self)
         self.connexion = conn  # réf. du socket de connexion
 
+    def parseargs(self, message):
+        m_quotes = message.split("\"")
+        m_parsed = []
+        i = 0
+        for m in m_quotes:
+            if i:
+                m_parsed.append(m)
+            else:
+                m_parsed += m.split(" ")
+            i += 1
+        m_parsed = [m for m in m_parsed if m != ""]
+        return m_parsed
+
+    def exec_command(self, args):
+        if args[0] == 'get':
+            return get(args)
+        if args[0] == 'put':
+            return put(args)
+        if args[0] == 'ls':
+            return ls(args)
+        if args[0] == 'rm':
+            return rm(args)
+        if args[0] == 'perm':
+            return perm(args)
+        return "Unknown command"
+
     # parse arguments from message
     # message : string
 
     def run(self):
         while 1:
-            result = exec_command(parseargs(input()))
-            message_emis = yaml.safe_dump(docexemple).encode("UTF-8")
-            self.connexion.send(message_emis)
+            result = self.exec_command(self.parseargs(input()))
+            self.connexion.send(result)
 
 
 # Programme principal - Établissement de la connexion :
@@ -78,7 +83,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     client_socket.connect((host, port))
-    #salt = client_socket.recv(2048)
+    # salt = client_socket.recv(2048)
     response = client_socket.recv(2048)
     # Input UserName
     name = input(response.decode('utf-8'))
@@ -102,6 +107,3 @@ try:
 except socket.error:
     print("La connexion a échoué.")
     sys.exit()
-
-
-
