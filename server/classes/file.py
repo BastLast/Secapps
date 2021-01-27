be7d743ab0d945be29a9dd5c470a9e66044eb839
Florian <florian.girardin42@gmail.com>
2021-01-27 11:25:24 +0100

    Update - Class file, folder, user

---
+++ server/classes/file.py
@@ -0,0 +1,18 @@
+class File():
+    name = ""
+    owner = ""
+    path = ""
+    list_user = []
+    '''List must be filled with item type dictionary of key/value'''
+    '''key of dictionary : pseudo@id, dl, send, delete, create, replace'''
+    '''value of dictionary : string, bool, bool, bool, bool, bool'''
+
+    '''Init principal'''
+    def __init__(self, name, owner, path, list):
+        self.name = name
+        self.owner = owner
+        self.path = path
+        self.list_user = list
+
+    def add_user_to_list(self, user):
+        self.list_user.append(user)
