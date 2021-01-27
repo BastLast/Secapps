6dccc019c2754cbd78e1d60dccff21f9e878061f
BastLast <bastien.last87@gmail.com>
2021-01-27 11:17:49 +0100

    added serialisation

--- server/server.py
+++ server/server.py
@@ -4,30 +4,36 @@
 HOST = '127.0.0.1'
 PORT = 40000
 
-import socket, sys, threading
+import socket
+import sys
+import threading
+import yaml
+
 
 class ThreadClient(threading.Thread):
-    '''objet thread pour gérer la connexion avec un client'''
+    # objet thread pour gérer la connexion avec un client
     def __init__(self, conn):
         threading.Thread.__init__(self)
         self.connexion = conn
 
     def run(self):
         # Dialogue avec le client :
-        nom = self.getName()        # Chaque thread possède un nom
+        nom = self.getName()  # Chaque thread possède un nom
         while 1:
-            msgClient = self.connexion.recv(1024).decode("utf-8")
-            if msgClient.upper() == "FIN" or msgClient =="":
+            receivedmessage = yaml.safe_dump(self.connexion.recv(1024).decode("UTF-8"))
+            receiveddoc = yaml.safe_load(receivedmessage)
+            if receivedmessage.upper() == "FIN" or receivedmessage == "":
                 break
-            message = "%s> %s" % (nom, msgClient)
-            print(message)
-
+            message = "%s> %s" % (nom, receivedmessage)
+            print(receiveddoc)
+            receiveddoc
 
         # Fermeture de la connexion :
-        self.connexion.close()      # couper la connexion côté serveur
-        del conn_client[nom]        # supprimer son entrée dans le dictionnaire
+        self.connexion.close()  # couper la connexion côté serveur
+        del conn_client[nom]  # supprimer son entrée dans le dictionnaire
         print("Client %s déconnecté." % nom)
         # Le thread se termine ici
+
 
 # Initialisation du serveur - Mise en place du socket :
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,16 +46,16 @@ print("Serveur prêt, en attente de requêtes ...")
 mySocket.listen(5)
 
 # Attente et prise en charge des connexions demandées par les clients :
-conn_client = {}                # dictionnaire des connexions clients
+conn_client = {}  # dictionnaire des connexions clients
 while 1:
     connexion, adresse = mySocket.accept()
     # Créer un nouvel objet thread pour gérer la connexion :
     th = ThreadClient(connexion)
     th.start()
     # Mémoriser la connexion dans le dictionnaire :
-    it = th.getName()        # identifiant du thread
+    it = th.getName()  # identifiant du thread
     conn_client[it] = connexion
-    print("Client %s connecté, adresse IP %s, port %s." %\
-           (it, adresse[0], adresse[1]))
+    print("Client %s connecté, adresse IP %s, port %s." % \
+          (it, adresse[0], adresse[1]))
     # Dialogue avec le client :
     connexion.send("Vous êtes connecté. Envoyez vos messages.".encode("utf-8"))
