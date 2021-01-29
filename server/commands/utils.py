89e7ad0e21b89125f4efdd3fdaa24d338814ce45
senetal <alex.senet38@gmail.com>
2021-01-29 14:45:13 +0100

    Add - utils.py that includes functions used by multiple commands (server-side)

---
+++ server/commands/utils.py
@@ -0,0 +1,20 @@
+#Functions used by multiple commands
+import json
+
+def is_owner(file,user):
+    return file.get("owner") == user
+
+def is_admin(user):
+    with open("users.json", "r") as users_file:
+        users = json.load(users_file)
+    return users.get(user).get("admin")
+
+def has_permission(file,user,permission):
+    if is_owner(file,user):
+        return True
+    if is_admin(user):
+        return permission=="list" or permission=="remove"
+    user_perm=file.get('permissions').get(user)
+    if not user_perm:
+        return False
+    return user_perm.get(permission)
