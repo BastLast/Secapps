64bae2c01d3b20960fe746fe17081c6a973bcaa1
BastLast <bastien.last87@gmail.com>
2021-02-28 16:26:33 +0100

    upload command is now saving file info in a json file

--- server/commands/put.py
+++ server/commands/put.py
@@ -1,5 +1,29 @@
+import base64
+import json
+
+
 def put(data, login):
+    defaultpermissions = {login: {
+        "c": "false",
+        "l": "true",
+        "d": "true",
+        "r": "false",
+        "o": "false"
+    }}
+    file = {
+        "content": base64.b64encode(data.get('serialized_file')).decode(),
+        "parent": login,
+        "owner": login,
+        "permissions": defaultpermissions
+    }
+    print(file)
+    with open("files/" + login + "/" + data.get('file_name') + ".json", "w") as outfile:
+        json.dump(file, outfile)
+
+    with open("files/" + login + "/" + data.get('file_name') + ".json", "r") as outfile:
+        datade = json.load(outfile)
+
     f = open("files/" + login + "/" + data.get('file_name'), 'wb')
-    f.write(data.get('serialized_file'))
+    f.write(base64.b64decode(datade.get("content").encode()))
     f.close()
     return "fichier recu et enregistré"
