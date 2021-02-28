ceb642747d50e645a4b0c2770ce3d37527360685
senetal <alex.senet38@gmail.com>
2021-02-28 16:21:48 +0100

    UPDATE - fixed rm command

--- server/commands/rm.py
+++ server/commands/rm.py
@@ -1,3 +1,28 @@
-def rm(data):
+import json
+import os
+from commands.utils import has_permission
 
-    return data.get('args')[0]
+def rm(data):
+    args = data.get('args')
+    user = data.get('user')
+
+    if len(args) != 2:
+        return "Error: Incorrect syntax.\nUsage: rm <file>"
+
+    if ".directory.json" in args[1]:
+        return "Error: No such file."
+
+    if "/" not in args[1]:
+        args[1] = user + "/" + args[1]
+
+    if not os.path.exists("files/" + args[1]):
+        return "Error: Error: No such file."
+
+    with open("files/"+args[1]) as file:
+        fileprop=json.load(file)
+
+    if not has_permission(fileprop, user, "r"):
+        return "Error: Permission denied."
+
+    os.remove("files/"+args[1])
+    return "Successfully removed "+args[1]
