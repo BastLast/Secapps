1e31f5afb99cf2f17e7cb2385989f126208d1022
BastLast <bastien.last87@gmail.com>
2021-02-28 20:44:00 +0100

    Ajout de la possibilité de save un fichier dans le rep d'un autre user

--- server/commands/put.py
+++ server/commands/put.py
@@ -1,8 +1,24 @@
 import base64
 import json
+import os.path
+from os import path
+
+from commands.utils import has_permission
+from commands.utils import is_owner
+from commands.utils import is_admin
 
 
 def put(data, login):
+    parent = login
+    if len(data.get("args")) > 2:
+        parent = data.get("args")[2]
+        if not path.exists("files/" + parent):
+            return "Erreur, le dossier cible n'existe pas"
+        with open("files/" + parent + "/.directory.json") as pfile:
+            parentproperties = json.load(pfile)
+            if not has_permission(parentproperties, login, "c"):
+                return "Vous n'avez pas la permission d'écrire ici !"
+
     defaultpermissions = {login: {
         "c": "false",
         "l": "true",
@@ -12,18 +28,18 @@ def put(data, login):
     }}
     file = {
         "content": base64.b64encode(data.get('serialized_file')).decode(),
-        "parent": login,
+        "parent": parent,
         "owner": login,
         "permissions": defaultpermissions
     }
     print(file)
-    with open("files/" + login + "/" + data.get('file_name') + ".json", "w") as outfile:
+    with open("files/" + parent + "/" + data.get('file_name') + ".json", "w") as outfile:
         json.dump(file, outfile)
 
-    with open("files/" + login + "/" + data.get('file_name') + ".json", "r") as outfile:
+    with open("files/" + parent + "/" + data.get('file_name') + ".json", "r") as outfile:
         datade = json.load(outfile)
 
-    f = open("files/" + login + "/" + data.get('file_name'), 'wb')
+    f = open("files/" + parent + "/" + data.get('file_name'), 'wb')
     f.write(base64.b64decode(datade.get("content").encode()))
     f.close()
     return "fichier recu et enregistré"
